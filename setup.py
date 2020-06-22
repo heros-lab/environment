4ffaadff05a8a49ba1053291023e46a768f40410
Gorillaion <h.iwasaki.015@gmail.com>
2020-06-22 20:08:43 +0900

    HERO

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='environment',
+    version="0.1.0",
+    url="https://github.com/heros-lab/environment",
+    description="My experiment environment for Proposed method with IRIP (2020.06)",
+    author='HERO',
+    license='MIT',
+    packages=find_packages(),
+    entry_points="""
+    [console_scripts]
+    environment = environment.environment:main
+    """,)
